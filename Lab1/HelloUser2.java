33439c43fa226cb036f2e9493d4c8e308ce1520e
Charishma Thota <cthota@ucsc.edu>
2018-04-04 00:09:28 -0700

    Adding Lab1

---
+++ Lab1/HelloUser2.java
@@ -0,0 +1,13 @@
+//-----------------------------------------------------------------------------
+// Charishma Thota
+// cthota
+// HelloUser2.java
+// Prints greeting to stdout
+//-----------------------------------------------------------------------------
+class HelloUser2{
+   public static void main( String[] args ){
+      String userName = System.getProperty("user.name");
+
+	System.out.println("Hello "+userName);
+	}
+}
